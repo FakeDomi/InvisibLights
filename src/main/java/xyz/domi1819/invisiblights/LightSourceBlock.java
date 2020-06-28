f1cb8aa600c90c0690660b4f5f3ca11e21ca68fc
Rongmario <ethanmahmood@gmail.com>
2020-06-28 02:52:54 +0100

    Work on drops + fixed rendering

--- src/main/java/xyz/domi1819/invisiblights/LightSourceBlock.java
+++ src/main/java/xyz/domi1819/invisiblights/LightSourceBlock.java
@@ -8,14 +8,20 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.storage.loot.LootContext;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Collections;
+import java.util.List;
 
 public class LightSourceBlock extends Block {
 
@@ -24,7 +30,7 @@ public class LightSourceBlock extends Block {
     private final BlockItem blockItem;
 
     public LightSourceBlock() {
-        super(Properties.create(Material.MISCELLANEOUS, DyeColor.YELLOW).hardnessAndResistance(0.2F).lightValue(15).sound(SoundType.CLOTH).variableOpacity());
+        super(Properties.create(Material.MISCELLANEOUS, DyeColor.YELLOW).doesNotBlockMovement().hardnessAndResistance(0.2F).lightValue(15).sound(SoundType.CLOTH));
         setRegistryName(InvisibLights.MOD_ID, "light_source");
         this.blockItem = new BlockItem(this, new Item.Properties().group(ItemGroup.DECORATIONS));
         this.blockItem.setRegistryName(InvisibLights.MOD_ID, "light_source");
@@ -43,26 +49,30 @@ public class LightSourceBlock extends Block {
     }
 
     @Override
-    public boolean isAir(BlockState state) {
-        return true;
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        return Collections.singletonList(new ItemStack(Items.GLOWSTONE_DUST, 2));
     }
 
     @Override
     @OnlyIn(Dist.CLIENT)
-    public boolean isSideInvisible(BlockState state, BlockState adjacentBlockState, Direction side) {
-        return isHidden;
+    public boolean isSideInvisible(BlockState state, BlockState adjacentState, Direction side) {
+        return isHidden || adjacentState.getBlock() == this;
     }
 
     @Override
-    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return context.hasItem(InvisibLights.LIGHT_ROD) || !isHidden && context != ISelectionContext.dummy() ? VoxelShapes.fullCube() : VoxelShapes.empty();
+    public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+        return isHidden ? VoxelShapes.empty() : VoxelShapes.fullCube();
     }
 
-    /*
     @Override
-    public BlockRenderType getRenderType(BlockState state) {
-        return isHidden ? BlockRenderType.INVISIBLE : BlockRenderType.MODEL;
+    @OnlyIn(Dist.CLIENT)
+    public float getAmbientOcclusionLightValue(BlockState state, IBlockReader world, BlockPos pos) {
+        return 1.0F;
     }
-     */
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, IBlockReader world, BlockPos pos) {
+        return true;
+    }
 
 }
